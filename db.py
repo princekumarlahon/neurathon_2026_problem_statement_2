67ac028f6fea1c80151bdc15fb2730115180ff15
princekumarlahon <princelahon456@gmail.com>
2026-02-15 15:49:50 +0530

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_db():
-    return mysql.connector.connect(
-        host="localhost",
-        user="user",
-        password="root",
-        database="biz_agent"
-    )
